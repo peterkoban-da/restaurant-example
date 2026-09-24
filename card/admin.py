76e02f077f13357dfe9cabdd387f58aea36aecdc
Peter Koban <peter.koban@developerakademie.com>
2026-09-24 11:33:50 +0200

    feat(card): add models

--- card/admin.py
+++ card/admin.py
@@ -1,3 +1,13 @@
 from django.contrib import admin
 
-# Register your models here.
+from card.models import Dish, Drink
+
+
+@admin.register(Dish)
+class AdminDish(admin.ModelAdmin):
+    pass
+
+
+@admin.register(Drink)
+class AdminDrink(admin.ModelAdmin):
+    pass
--- card/models.py
+++ card/models.py
@@ -1,3 +1,28 @@
 from django.db import models
 
+
 # Create your models here.
+class CardItem(models.Model):
+    """
+    Model for basic Card Item
+    """
+
+    name = models.CharField(max_length=150)
+    price = models.FloatField()
+    lunch_action = models.BooleanField(default=False)
+
+
+class Dish(CardItem):
+    """
+    Model for Dish
+    """
+
+    is_vegan = models.BooleanField(default=False)
+
+
+class Drink(CardItem):
+    """
+    Model for Drinks
+    """
+
+    is_alcoholic = models.BooleanField(default=False)
